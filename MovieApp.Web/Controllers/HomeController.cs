87ebe692828193311add257e2da216113b5d0e7a
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-10-31 03:01:00 +0300

    Add project files.

---
+++ MovieApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System.Collections.Generic;
+
+namespace MovieApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string filmBasligi = "film başlığı";
+            string filmAciklama = "filmin açıklaması";
+            string filmYonetmen = "filmin yönetmen adı";
+            string[] oyuncular = { "oyuncu 1", "oyuncu 2","oyuncu 3","oyuncu 4" };
+
+            var m = new Movie();
+
+            m.Title = filmBasligi;
+            m.Description = filmAciklama;
+            m.Director = filmYonetmen;
+            m.Players = oyuncular;
+            m.ImageUrl = "1.jpg";
+
+            return View(m);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    // controller
+    public class MoviesController : Controller
+    {
+        // action
+        // localhost:5000/movies/list
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            var filmListesi = new List<Movie>()
+            {
+                new Movie { 
+                    Title="film 1",
+                    Description="açıklama 1", 
+                    Director="Yönetmen 1", 
+                    Players=new string[] { "oyuncu 1","oyuncu 2"},
+                    ImageUrl="1.jpg"
+                },
+                new Movie { 
+                    Title="film 2",
+                    Description="açıklama 1", 
+                    Director="Yönetmen 1", 
+                    Players=new string[] { "oyuncu 1","oyuncu 2"},
+                    ImageUrl="2.jpg"
+                },
+                new Movie { 
+                    Title="film 3",
+                    Description="açıklama 1", 
+                    Director="Yönetmen 1", 
+                    Players=new string[] { "oyuncu 1","oyuncu 2"},
+                    ImageUrl="3.jpg"
+                }
+
+            };
+
+            var model = new MovieGenreViewModel()
+            {
+                Movies = filmListesi
+            };
+            
+            return View("Movies", model);
+        }
+
+        // localhost:5000/movies/details
+        public string Details()
+        {
+            return "Film Detayı";
+        }
+    }
+}
